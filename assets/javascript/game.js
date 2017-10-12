5f9f996d0697ca4f11191f94df4eaebb878b3a0b
Claire Pate <clairepate@Claires-Air.attlocal.net>
2017-10-11 21:03:15 -0400

    started javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessSoFar = [];
+var words [
+	"football",
+	"field",
+	"touchdown",
+	"omaha",
+	"panthers",
+	"defense",
+	"ref",
+	"mascot"];
+
+
+
+// FUNCTIONS==============================================
+
+// updating the wins
+
+// updating the losses
+
+// counting the remaining guesses
+
+// Listing the users guesses
+
+// reset function
+var reset = function () {
+	guessesLeft = 10;
+	guessSoFar = [];
+};
